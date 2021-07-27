e7c779edd77ca071a4b7b9f5fe6203fc82d8b8a8
supran28 <87807595+supran28@users.noreply.github.com>
2021-07-27 13:45:31 +0700

    Add files via upload

--- tugas-array.js
+++ tugas-array.js
@@ -26,7 +26,18 @@
 //document.write("Java Script Divison =" + JavaScriptDivison.toString())
 
 
-var santriPondok =[" ", "Wandi","Aldi","Acep","Putra","Hafif","Syofyan","Rahmad"]
-for(var i = 1; i < santriPondok.length; i++){
- console.log(i + "." + " " + santriPondok[i])
+//var santriPondok =[" ", "Wandi","Aldi","Acep","Putra","Hafif","Syofyan","Rahmad"]
+//for(var i = 1; i < santriPondok.length; i++){
+// console.log(i + "." + " " + santriPondok[i])
+//}
+
+var nilaiSantri =[ 5, 9, 6, 7, 9, 8, 10, 7, 8]
+
+var jumlahNilai = 0;
+
+for(var n = 0; n < nilaiSantri.length; n++){
+    jumlahNilai += nilaiSantri[n]
 }
+
+console.log(jumlahNilai)
+console.log(jumlahNilai/nilaiSantri.length)
