2c610d4a265a77e6725da97ad05dfd00e2077266
supran28 <87807595+supran28@users.noreply.github.com>
2021-07-27 11:52:38 +0700

    Add files via upload

---
+++ tugas-array.js
@@ -0,0 +1,22 @@
+var listKelurahanBantul = [
+                        ["Mulyodadi", "Sidomulyo", "Sumbermulyo"], 
+                        ["Banguntapan", "Baturetno", "Jagalan","Jambidan", "Potorono", "Singosaren", "Tamanan", "Wirokerten"],  
+                        ["Bantul", "Palbapang", "Ringinharjo", "Sabdodadi", "Trirenggo"],
+                        ["Dlingo", "Jatimulyo", "Mangunan", "Muntuk", "Temuwuh", "Terong"],
+                        ["Girirejo", "Imogiri", "Karangtalun", "Karangtengah", "Kebonagung", "Selopamioro", "Sriharjo", "Wukirsari"],
+                        ["Canden", "Patalan", "Sumberagung", "Trimulyo"],
+                        ["Bangunjiwo","Ngestiharjo", "Tamantirto", "Tirtonirmolo"],
+                        ["Donotirto", "Parangtritis", "Tirtohargo", "Tirtomulyo", "Tirtosari"],
+                        ["Guwosari", "Sendangsari", "Triwidadi"],
+                        ["Caturharjo", "Gilangharjo", "Triharjo", "Wijirejo"],
+                        ["Srimulyo", "Sitimulyo", "Srimartani"],
+                        ["Bawuran", "Pleret","Segoroyo", "soWonokromo", "Wonolelo"],
+                        ["Panjangrejo", "Bangunharjo", "Srihardono"],
+                        ["Gadingsari", "Gadingharjo", "Murtigading", "Srigading"],
+                        ["Argodadi", "Argorejo", "Argosari", "Argomulyo"],
+                        ["Bangunharjo", "Panggungharjo", "Pendowoharjo", "Timbulharjo"],
+                        ["Poncosari", "Trimurti"],
+                        ]  
+
+                         
+
