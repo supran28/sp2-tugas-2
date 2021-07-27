d48022ea47f32e33a0c1fb96c3dd0c7dfd87d0ee
supran28 <87807595+supran28@users.noreply.github.com>
2021-07-27 13:00:52 +0700

    Add files via upload

--- tugas-array.js
+++ tugas-array.js
@@ -1,22 +1,26 @@
-var listKelurahanBantul = [
-                        ["Mulyodadi", "Sidomulyo", "Sumbermulyo"], 
-                        ["Banguntapan", "Baturetno", "Jagalan","Jambidan", "Potorono", "Singosaren", "Tamanan", "Wirokerten"],  
-                        ["Bantul", "Palbapang", "Ringinharjo", "Sabdodadi", "Trirenggo"],
-                        ["Dlingo", "Jatimulyo", "Mangunan", "Muntuk", "Temuwuh", "Terong"],
-                        ["Girirejo", "Imogiri", "Karangtalun", "Karangtengah", "Kebonagung", "Selopamioro", "Sriharjo", "Wukirsari"],
-                        ["Canden", "Patalan", "Sumberagung", "Trimulyo"],
-                        ["Bangunjiwo","Ngestiharjo", "Tamantirto", "Tirtonirmolo"],
-                        ["Donotirto", "Parangtritis", "Tirtohargo", "Tirtomulyo", "Tirtosari"],
-                        ["Guwosari", "Sendangsari", "Triwidadi"],
-                        ["Caturharjo", "Gilangharjo", "Triharjo", "Wijirejo"],
-                        ["Srimulyo", "Sitimulyo", "Srimartani"],
-                        ["Bawuran", "Pleret","Segoroyo", "soWonokromo", "Wonolelo"],
-                        ["Panjangrejo", "Bangunharjo", "Srihardono"],
-                        ["Gadingsari", "Gadingharjo", "Murtigading", "Srigading"],
-                        ["Argodadi", "Argorejo", "Argosari", "Argomulyo"],
-                        ["Bangunharjo", "Panggungharjo", "Pendowoharjo", "Timbulharjo"],
-                        ["Poncosari", "Trimurti"],
-                        ]  
-
+//var listKelurahanBantul = [
+//                        ["Mulyodadi", "Sidomulyo", "Sumbermulyo"], 
+//                        ["Banguntapan", "Baturetno", "Jagalan","Jambidan", "Potorono", "Singosaren", "Tamanan", "Wirokerten"],  
+//                        ["Bantul", "Palbapang", "Ringinharjo", "Sabdodadi", "Trirenggo"],
+//                        ["Dlingo", "Jatimulyo", "Mangunan", "Muntuk", "Temuwuh", "Terong"],
+//                        ["Girirejo", "Imogiri", "Karangtalun", "Karangtengah", "Kebonagung", "Selopamioro", "Sriharjo", "Wukirsari"],
+//                        ["Canden", "Patalan", "Sumberagung", "Trimulyo"],
+//                        ["Bangunjiwo","Ngestiharjo", "Tamantirto", "Tirtonirmolo"],
+//                        ["Donotirto", "Parangtritis", "Tirtohargo", "Tirtomulyo", "Tirtosari"],
+//                        ["Guwosari", "Sendangsari", "Triwidadi"],
+//                        ["Caturharjo", "Gilangharjo", "Triharjo", "Wijirejo"],
+//                        ["Srimulyo", "Sitimulyo", "Srimartani"],
+//                        ["Bawuran", "Pleret","Segoroyo", "soWonokromo", "Wonolelo"],
+//                        ["Panjangrejo", "Bangunharjo", "Srihardono"],
+//                        ["Gadingsari", "Gadingharjo", "Murtigading", "Srigading"],
+//                        ["Argodadi", "Argorejo", "Argosari", "Argomulyo"],
+//                        ["Bangunharjo", "Panggungharjo", "Pendowoharjo", "Timbulharjo"],
+//                        ["Poncosari", "Trimurti"],
+//                        ]  
+//
                          
-
+var reactNative =["Hafif","Dayat","Wandi"];
+var reactJs =["Yusuf","Luthfi","Tompel"];
+var JavaScriptDivison = reactNative.concat(reactJs);
+console.log("Java Script Divison =" + JavaScriptDivison.toString())
+document.write("Java Script Divison =" + JavaScriptDivison.toString())
